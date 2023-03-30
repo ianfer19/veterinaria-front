8a6eff538128472491c6f03ca5d99a50c339eeea
ianfer19 <ianfer09col@gmail.com>
2023-03-29 21:21:01 -0500

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PacientesComponent } from './components/pacientes/pacientes.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { PersonaComponent } from './components/persona/persona.component';
+import { BarraComponent } from './components/barra/barra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PacientesComponent,
+    AuthComponent,
+    PersonaComponent,
+    BarraComponent
   ],
   imports: [
     BrowserModule,
